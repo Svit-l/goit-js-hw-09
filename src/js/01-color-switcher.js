d99577574f28e63f9c70f6cb9e56f25231a1b64d
Svit-l <lov.svit@gmail.com>
2021-11-02 01:35:04 +0200

    Задача 1 - изменение цвета по нажатию

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onButtonClick = () => {
+  const color = getRandomHexColor();
+  // console.log(color);
+  document.body.style.backgroundColor = color;
+};
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    //   const bodyStyle =
+    onButtonClick();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
