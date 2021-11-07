66165242985e52e1e9c4d12ff31cb59678ee5dc9
Svit-l <lov.svit@gmail.com>
2021-11-07 23:00:46 +0200

    Задача 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,36 @@
+// поиск элементов
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+//
 let timerId = null;
+// кнопка стоп - не активна
+stopBtn.setAttribute('disabled', 'true');
 
+// генерация случайного цвета
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// генерирование цвета
 const onButtonClick = () => {
   const color = getRandomHexColor();
-  // console.log(color);
   document.body.style.backgroundColor = color;
 };
 
+// нажатие на кнопку старт - запуск изменения цвета через интервал 1с.
+// кнопка старт - не активна
 startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
   timerId = setInterval(() => {
-    //   const bodyStyle =
     onButtonClick();
   }, 1000);
 });
 
+// нажатие на кнопку стоп - остановка изменения цвета
+// кнопка стоп - не активна
 stopBtn.addEventListener('click', () => {
+  stopBtn.setAttribute('disabled', 'true');
+  startBtn.removeAttribute('disabled');
   clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// В HTML есть разметка формы,
+// в поля которой пользователь будет вводить первую задержку в миллисекундах,
+// шаг увеличения задержки для каждого промиса после первого
+// и количество промисов которое необходимо создать.
+
+// Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз,
+//   сколько ввели в поле amount.
+//   При каждом вызове передай ей номер создаваемого промиса(position)
+//   и задержку учитывая введенную пользователем первую задержку(delay) и шаг(step).
+
+const delay = document.getElementsByName('delay');
+const step = document.getElementsByName('step');
+const amount = document.getElementsByName('amount');
+
+console.log(delay);
+console.log(step);
+console.log(amount);
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// const promise = createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
