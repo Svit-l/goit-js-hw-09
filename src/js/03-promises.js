948b4cdca49055a03852685ccb5f6a29beeb04bd
Svit-l <lov.svit@gmail.com>
2021-11-08 03:07:08 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // В HTML есть разметка формы,
 // в поля которой пользователь будет вводить первую задержку в миллисекундах,
 // шаг увеличения задержки для каждого промиса после первого
@@ -11,10 +12,30 @@
 const delay = document.getElementsByName('delay');
 const step = document.getElementsByName('step');
 const amount = document.getElementsByName('amount');
+const form = document.querySelector('.form');
 
-console.log(delay);
-console.log(step);
-console.log(amount);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const values = { position, delay };
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(values);
+      }
+      reject(values);
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+// createPromise.then(result => console.log(result)).catch(result => console.log(result));
 
 // function createPromise(position, delay) {
 //   const shouldResolve = Math.random() > 0.3;
@@ -25,10 +46,29 @@ console.log(amount);
 //   }
 // }
 
-// const promise = createPromise(2, 1500)
+// createPromise(2, 1500)
 //   .then(({ position, delay }) => {
 //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
 //   })
 //   .catch(({ position, delay }) => {
 //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
+
+const formSubmit = form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const inputDelay = Number(form.delay.value);
+  const inputStep = Number(form.step.value);
+  const inputAmount = Number(form.amount.value);
+  console.log(inputDelay);
+  console.log(inputStep);
+  console.log(inputAmount);
+  let delay = inputDelay;
+  for (let i = 0; i < inputAmount; i++) {
+    createPromise();
+    new Promise(resolve => {
+      setTimeout(() => resolve(i), delay);
+      console.log(`✅ Fulfilled promise ${i + 1} in ${delay}ms`);
+    }, inputDelay);
+    delay += inputStep;
+  }
+});
